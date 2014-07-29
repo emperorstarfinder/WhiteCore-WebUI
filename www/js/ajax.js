c19fac1f8b1e8e366e71c797f8e8124efb22f2d5
EmperorStarfinder <emperor@secondgalaxy.com>
2014-07-29 18:29:57 -0400

    cleanup ajax.js to make sure it is clean code.

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -1,12 +1,11 @@
-function ajaxObj( meth, url ) {
+function ajaxObj(meth, url){
 	var x = new XMLHttpRequest();
 	x.open( meth, url, true );
-	x.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	x.netRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	return x;
 }
 
 function ajaxReturn(x){
 	if(x.readyState == 4 && x.status == 200){
-		return true;	
-		}
+	}
 }
