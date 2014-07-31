077eac964569ce7cc509ea09936233df4603fa6d
EmperorStarfinder <emperor@secondgalaxy.com>
2014-07-30 22:11:16 -0400

    Commit the initial style and template for the webUI

---
+++ www/templates/template_pageBottom.php
@@ -0,0 +1,4 @@
+<div id="pageBottom">
+   <p>&copy;2014 WhiteCore-Sim.org</p>
+   <p>Initial design of this web UI was done by the Second Galaxy Development Team and donated to the WhiteCore-Sim opensource project.</p>
+</div>
